a2511e27ab4108b501eff6625a47b6695c14831a
sjmin <msj3001@naver.com>
2022-11-02 14:38:03 +0900

    HackerRank

---
+++ HackerRank/personality.py
@@ -0,0 +1,16 @@
+n = int(input())
+arr = []
+for i in range(n):
+   arr_n = int(input())
+   arr.append(arr_n)
+k = int(input())
+
+if k in arr:
+    result = "YES"
+    print(result)
+else:
+    result = "NO"
+    print(result)
+
+
+
