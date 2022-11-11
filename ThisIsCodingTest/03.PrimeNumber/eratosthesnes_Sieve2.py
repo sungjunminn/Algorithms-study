5c863c457766504b74e0bdeba9a31b2f4e95a45b
sjmin <msj3001@naver.com>
2022-11-11 16:36:30 +0900

    Eratothesnes

--- ThisIsCodingTest/03.PrimeNumber/eratosthesnes_Sieve2.py
+++ ThisIsCodingTest/03.PrimeNumber/eratosthesnes_Sieve2.py
@@ -18,7 +18,7 @@ def countPrimes2(n):
     sieve = [False, False] + [True] * (n - 1)
     count = 0
     for i in range(2, n + 1):
-        if (sieve[i]):
+        if (sieve[i]) == True:
             count += 1
         for j in range(i * 2, n + 1, i):
             sieve[j] = False
@@ -31,3 +31,4 @@ elapsed = time.time() - start
 print(elapsed, '초')
 print(elapsed/60, '분')
 print(elapsed/60/60, '시간')
+
