5dfc4c9dcb5b21da2fe02ccddfaddc193dbd7785
sjmin <msj3001@naver.com>
2022-11-21 11:14:04 +0900

    implementation

---
+++ ThisIsCodingTest/08.Implementation/KnightOfKingdom1.py
@@ -0,0 +1,17 @@
+data = input()
+column = int(chr(ord(data[0]) - 48))
+row = int(data[1])
+
+dx = [-2, -2, 2, 2, -1, 1, -1, 1]
+dy = [-1, 1, -1, 1, -2, -2, 2, 2]
+count = 0
+
+for i in range(8):
+    nx = column + dx[i]
+    ny = row + dy[i]
+    # 공간을 벗어나는 경우 무시
+    if nx > 0 and ny > 0 and nx <= 8 and ny <= 8:
+        count += 1
+
+print(count)
+
