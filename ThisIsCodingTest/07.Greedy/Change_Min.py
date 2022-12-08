321dda3b2d59044a9a9f882e68236ccc54d09fd8
sjmin <msj3001@naver.com>
2022-12-08 15:51:42 +0900

    Greedy practice

--- ThisIsCodingTest/07.Greedy/Change_Min.py
+++ ThisIsCodingTest/07.Greedy/Change_Min.py
@@ -5,6 +5,7 @@ count = 0
 coin_types = [500, 100, 50, 10]
 
 for coin in coin_types:
-    count += n // coin # 해당 화폐로 거슬러 줄 수 있는 동전의 개수 세기
+    # 해당 화폐로 거슬러 줄 수 있는 동전의 개수 세기
+    count += n // coin
     n %= coin
 print(count)
