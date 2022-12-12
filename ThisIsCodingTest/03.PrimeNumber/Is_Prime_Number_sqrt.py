b070366ced66b25bf6b3f8cb50af675ddc1191bc
sjmin <msj3001@naver.com>
2022-12-12 10:29:52 +0900

    PrimeNumber

--- ThisIsCodingTest/03.PrimeNumber/Is_Prime_Number_sqrt.py
+++ ThisIsCodingTest/03.PrimeNumber/Is_Prime_Number_sqrt.py
@@ -2,8 +2,10 @@ import math
 
 # 소수 판별 함수
 def is_prime_number(x):
-    for i in range(2, int(math.sqrt(x)) + 1): # 2부터 x의 제곱근까지의 모든 수를 확인하며
-        if x % i == 0: # x가 해당 수로 나누어 떨어진다면
+    # 2부터 x의 제곱근까지의 모든 수를 확인하며
+    for i in range(2, int(math.sqrt(x)) + 1):
+        # x가 해당 수로 나누어 떨어진다면
+        if x % i == 0:
             return False # 소수가 아님
     return True # 소수임
 
