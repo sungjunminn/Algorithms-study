21c50e153c48b294fe65b8c46720270deda781ef
82102 <msj3001@naver.com>
2022-12-18 22:41:14 +0900

    .

--- HackerRank/Color.py
+++ HackerRank/Color.py
@@ -8,24 +8,24 @@ def solution(cost, x):
             for next in solution(cost[i+1:], x-1):
                 yield [cost[i]] + next
 
-# if __name__ == '__main__':
-#     fptr = open(os.environ['OUTPUT_PATH'], 'w')
-# 
-#     cost_count  = int(input().strip())
-#
-#     cost = []
-#
-#     for _ in range(cost_count):
-#         cost_item = int(input().strip())
-#         cost.append(cost_item)
-#
-#     x = int(input().strip())
-#
-#     result = solution(cost, x)
-#
-#     fptr.write(str(result) + '\n')
-#
-#     fptr.close()
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    cost_count  = int(input().strip())
+
+    cost = []
+
+    for _ in range(cost_count):
+        cost_item = int(input().strip())
+        cost.append(cost_item)
+
+    x = int(input().strip())
+
+    result = solution(cost, x)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
 
 # for i in solution([3, 4, 1], 8):
 #     print(i, end=" ")
