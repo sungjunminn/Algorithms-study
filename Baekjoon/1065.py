ae3f74fbedbf21aa837f65a82406ba2c0accf7f9
sjmin <msj3001@naver.com>
2023-01-02 17:32:46 +0900

    hansu

--- Baekjoon/1065.py
+++ Baekjoon/1065.py
@@ -5,7 +5,7 @@ def hansu(num):
         if i < 100:
             hansu_cnt += 1  # 100보다 작으면 모두 한수
         elif num_list[0]-num_list[1] == num_list[1]-num_list[2]:
-            hansu_cnt += 1  # x의 각 자리가 등차수열이면 한수
+            hansu_cnt += 1  # x의 각 자리가 등차 수열이면 한수
     return hansu_cnt
 
 num = int(input())
